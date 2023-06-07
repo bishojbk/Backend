0f42439a62a29003ed7f6f33a4080f24d2916973
bishojbk <bishojbk@gmail.com>
2023-06-07 11:58:27 +0545

    Modules and require

---
+++ people.js
@@ -0,0 +1,8 @@
+const people = ["EJR", "JER", "EMINEM"];
+const ages = ["2", "3", "4", "5", "6", "7"];
+
+module.exports = {
+  people: people,
+  ages,
+  // people:people can also be written as people.(just as in ages)
+};
