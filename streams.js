95e7da2b59e069e725770996c25c6db2a78cffe5
bishojbk <bishojbk@gmail.com>
2023-06-08 16:00:56 +0545

    Day2: Piping

--- streams.js
+++ streams.js
@@ -3,11 +3,11 @@ const fs = require("fs");
 const readStream = fs.createReadStream("./text.txt", { encoding: "utf8" });
 const writeStream = fs.createWriteStream("./new_text.txt");
 
-readStream.on("data", (chunk) => {
-  console.log(chunk);
+// readStream.on("data", (chunk) => {
+//   console.log(chunk);
 
-  writeStream.write(chunk);
-});
+//   writeStream.write(chunk);
+// });
 
 //it can also be translated into a string by:
 // const readStream = fs.createReadStream("./text.txt");
@@ -15,3 +15,6 @@ readStream.on("data", (chunk) => {
 // readStream.on("data", (chunk) => {
 //   console.log(chunk.toString());
 // });
+
+//piping
+readStream.pipe(writeStream);
