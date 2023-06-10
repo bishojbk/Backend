25e361f309725a2af58996483812e778ecf32f1c
bishojbk <bishojbk@gmail.com>
2023-06-10 07:04:45 +0545

    Day3: Servers and Clients

--- people.js
+++
@@ -1,8 +0,0 @@
-const people = ["EJR", "JER", "EMINEM"];
-const ages = ["2", "3", "4", "5", "6", "7"];
-
-module.exports = {
-  people: people,
-  ages,
-  // people:people can also be written as people.(just as in ages)
-};
