b648b6c99fd35f3210aa8a981c369ee4006f5054
bishojbk <bishojbk@gmail.com>
2023-06-10 07:38:08 +0545

    Day3: writing HTML into the browser

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log("Server created");
+  console.log(req.url, req.method);
+
+  res.setHeader("Content", "text/html");
+
+  fs.readFile("./html/index.html", (err, data) => {
+    if (err) {
+      console.log("Error");
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(3000, () => {
